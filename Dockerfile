4c255fdaf71844c8acaabf791cab2376d89e9863
Shisei Hanai <ruimo.uno@gmail.com>
2015-02-05 12:13:44 +0900

    Add psycopg2.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ MAINTAINER Shisei Hanai<ruimo.uno@gmail.com>
 
 RUN apt-get update
 RUN apt-get -y install python3-pip libpq-dev
+RUN pip3 install psycopg2
 RUN pip3 install Django==1.7.4
 
 RUN django-admin startproject hello
--- client-postgres.sh
+++ client-postgres.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker -p 6502:6502 run --rm --link postgres:postgres --entrypoint="/bin/bash" -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -v $HOME:/var/home:rw -i -t ruimo/df-ub1404-django --rcfile /profile -i
+docker run -p 6502:6502 --rm --link postgres:postgres --entrypoint="/bin/bash" -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -v $HOME:/var/home:rw -i -t ruimo/df-ub1404-django --rcfile /profile -i
